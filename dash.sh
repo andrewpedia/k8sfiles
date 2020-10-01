de3d5adf743f4f0818026c74967085eb55d143d3
Your Name <you@example.com>
2020-10-01 08:08:30 +0000

    some useful files to boot up k8s cluster

---
+++ dash.sh
@@ -0,0 +1,2 @@
+#!/bin/zsh
+kubectl proxy --address='0.0.0.0'
---
+++ vm.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+ vboxmanage startvm w1 --type headless
+ vboxmanage startvm w2 --type headless
+ vboxmanage startvm w3 --type headless
